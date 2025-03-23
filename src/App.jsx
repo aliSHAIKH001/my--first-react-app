6a6fcc8e73811206aae21a14e4e230498eb693be
Ali Shaikh <shahinshaikh603@gmail.com>
2025-03-24 04:19:14 +0530

    feat: add MovieCard and Spinner components, implement debounced search feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import Search from "./components/search"
 import {useEffect, useState} from 'react' 
+import {useDebounce} from 'react-use'
+
+import Search from "./components/search"
+import Spinner from "./components/spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -17,16 +21,23 @@ const app = () => {
 
   // We make the search state here so it is tied to main scope
   const [searchTerm, setSearchTerm] = useState('');
-  const [erroMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
-  const fetchMovies = async () => {
+  // Debounce the search term too prevent the user from making too many API requests
+  // By waiting for the user to stop typing for 500ms  
+  useDebounce(() => setDebouncedSearchTerm(searchTerm, 500, [searchTerm]));
+
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
 
     try{
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query 
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       
       const response = await fetch(endpoint, API_OPTIONS);
 
@@ -53,8 +64,8 @@ const app = () => {
   }
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return ( 
     <main>
@@ -69,7 +80,19 @@ const app = () => {
         </header>
 
         <section className="all-movies">
-          <h2>All Movies</h2>
+          <h2 className="mt-[40px]" >All Movies</h2>
+
+          { isLoading ? (
+            <Spinner/>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ): (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
         </section>
 
       </div>
