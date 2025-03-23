ebc5e98a09dcf59bb328c5c34e1df7d4e6dc4485
Ali Shaikh <shahinshaikh603@gmail.com>
2025-03-24 02:13:31 +0530

    feat: add async function to fetch movies from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,77 @@
-import React from "react"
 import Search from "./components/search"
-import {useState} from 'react' 
+import {useEffect, useState} from 'react' 
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const app = () => {
 
   // We make the search state here so it is tied to main scope
   const [searchTerm, setSearchTerm] = useState('');
+  const [erroMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try{
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok) {
+        throw new Error("Failed to fetch movies.");
+      }
+
+      const data = await response.json();
+
+      if(data.Response === 'false') {
+        setErrorMessage(data.Error || "Failed To Fetch Movies");
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+
+    } catch(error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage("Error fetching movies, Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return ( 
     <main>
       <div className="pattern" /> 
 
       <div className="wrapper">
+
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className="text-gradient">Movies</span> You Will Enjoy Without The Hassle</h1>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <p className="text-3xl text-white">{searchTerm}</p>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+        </section>
+
       </div>
     </main>
    );
